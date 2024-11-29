9cb7fbaa5216db2a9829ebd20a08a3575ddd718e
KDDilshan <kdprojects847@gmail.com>
2024-11-29 21:40:58 +0530

    added project

---
+++ src/main/java/com/kavindu/land_selling/LandSellingApplication.java
@@ -0,0 +1,13 @@
+package com.kavindu.land_selling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LandSellingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LandSellingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kavindu/land_selling/LandSellingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kavindu.land_selling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LandSellingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
