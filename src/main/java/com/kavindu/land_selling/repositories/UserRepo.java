62b8b36aeaf0b4957f8429be25c61094ca72d6c7
KDDilshan <kdprojects847@gmail.com>
2025-01-28 08:23:16 +0530

    remove unvanted files

--- src/main/java/com/kavindu/land_selling/repositories/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package com.kavindu.land_selling.repositories;
-
-import com.kavindu.land_selling.entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepo extends JpaRepository<User, Integer> {
-}
