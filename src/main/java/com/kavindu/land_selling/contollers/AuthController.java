19e2d851ff1713f61e2d051a432a90dd07c5a179
KDDilshan <kdprojects847@gmail.com>
2025-01-30 14:27:42 +0530

    add register and register controller

--- src/main/java/com/kavindu/land_selling/contollers/AuthController.java
+++ src/main/java/com/kavindu/land_selling/contollers/AuthController.java
@@ -1,9 +1,35 @@
 package com.kavindu.land_selling.contollers;
 
+import com.kavindu.land_selling.dto.request.UserReqDto;
+import com.kavindu.land_selling.services.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
-@RequestMapping("/api/v1/")
+@RequestMapping("/auth/api/")
 public class AuthController {
+
+    private final UserService userService;
+    public AuthController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping(value = "/register")
+    public ResponseEntity<?> register(@RequestBody UserReqDto userReqDto) {
+        try{
+            String Response= userService.register(userReqDto.getUsername(),userReqDto.getPassword(),userReqDto.getEmail(),userReqDto.getPhone());
+            return new ResponseEntity<>(Response,HttpStatus.CREATED);
+        }catch (Exception e){
+            Map<String,String> map = new HashMap<>();
+            map.put("error",e.getMessage());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(map);
+        }
+    }
 }
--- src/main/java/com/kavindu/land_selling/services/UserService.java
+++ src/main/java/com/kavindu/land_selling/services/UserService.java
@@ -1,7 +1,38 @@
 package com.kavindu.land_selling.services;
 
+import com.kavindu.land_selling.entities.AppUser;
+import com.kavindu.land_selling.repositories.RefreshRepositoty;
+import com.kavindu.land_selling.repositories.UserRepository;
+import com.kavindu.land_selling.security.JwtGenerator;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+    private final UserRepository userRepository;
+    private final RefreshRepositoty refreshRepositoty;
+    private final JwtGenerator jwtGenerator;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder=new BCryptPasswordEncoder();
+
+
+    public UserService(UserRepository userRepository, RefreshRepositoty refreshRepositoty, JwtGenerator jwtGenerator) {
+        this.userRepository = userRepository;
+        this.refreshRepositoty = refreshRepositoty;
+        this.jwtGenerator = jwtGenerator;
+    }
+
+    public String register(String username, String password,String email,String phoneNo) {
+        if(userRepository.findByUsername(username)!=null) {
+            return "Username is already in use";
+        }
+        AppUser appUser=new AppUser();
+        appUser.setUsername(username);
+        appUser.setPassword(bCryptPasswordEncoder.encode(password));
+        appUser.setEmail(email);
+        appUser.setPhone(phoneNo);
+        userRepository.save(appUser);
+
+        return "User Registerd Successfully";
+
+    }
 }
