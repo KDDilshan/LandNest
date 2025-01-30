2c70b6a2b97702cca6effac49e3911eea542c409
KDDilshan <kdprojects847@gmail.com>
2025-01-30 14:28:25 +0530

    add security config with jwt secret

--- src/main/java/com/kavindu/land_selling/config/SecurityConfig.java
+++ src/main/java/com/kavindu/land_selling/config/SecurityConfig.java
@@ -1,4 +1,35 @@
 package com.kavindu.land_selling.config;
 
+import com.kavindu.land_selling.security.JwtAuthenticationFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
 public class SecurityConfig {
+    private final JwtAuthenticationFilter authenticationFilter;
+
+    public SecurityConfig(JwtAuthenticationFilter authenticationFilter) {
+        this.authenticationFilter = authenticationFilter;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf->csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/auth/api/register").permitAll()
+                        .anyRequest().authenticated())
+                .formLogin(form ->form.permitAll())
+                .addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 }
